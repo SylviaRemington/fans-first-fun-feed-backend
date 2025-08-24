135f836745ee8b7a89f856eae061334651508619
SylviaRemington <sylviaremington@gmail.com>
2025-08-23 19:07:06 -0700

    addtl spacing added for better readability

--- models/user.js
+++ models/user.js
@@ -22,11 +22,12 @@ module.exports = mongoose.model('User', userSchema);
 
 
 
+
+
+
 /*
 Thanks to the verifyToken middleware function, all protected routes in this application will have access
  to an object representing the logged in user, through the req object. For these purposes, the most relevant 
  properties on the user object will be its ObjectId (_id) and the username. Additionally, the User model will 
  be referenced by funmoment documents.
 */
-
-
