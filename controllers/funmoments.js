a683f8aaa0ded94121cc1e1b06b2b0eac2bc769f
SylviaRemington <sylviaremington@gmail.com>
2025-08-25 02:44:27 -0700

    changing model names

--- controllers/funmoments.js
+++ controllers/funmoments.js
@@ -14,7 +14,7 @@
 
 const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
-const FunMoment = require("../models/funmoment.js");
+const FunMoment = require("../models/FunMoment.js");
 const router = express.Router();
 
 // ADD ROUTES HERE
