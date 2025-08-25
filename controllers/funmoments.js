97e709f15f490f5a7a684e2621c462623c6188d7
SylviaRemington <sylviaremington@gmail.com>
2025-08-24 19:51:00 -0700

    creating a funmoments router

---
+++ controllers/funmoments.js
@@ -0,0 +1,10 @@
+// controllers/hoots.js
+
+const express = require("express");
+const verifyToken = require("../middleware/verify-token.js");
+const FunMoment = require("../models/funmoment.js");
+const router = express.Router();
+
+// add routes here
+
+module.exports = router;
