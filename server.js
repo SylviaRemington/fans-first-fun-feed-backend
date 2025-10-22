6179c7d0aa40f808d32d62ae260091cbe5890a7c
SylviaRemington <sylviaremington@gmail.com>
2025-10-22 11:07:45 -0700

    addtl spacing

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ const PORT= process.env.PORT
 // Start the server and listen on port 3000
 app.listen(PORT, () => {
 
+
+  
   // keeping this console log in so that I'm aware that my express app is ready & isn't crashing. This is for awareness of terminal and of app working. This is necessary for an effective app!
   console.log('The express app is ready!');
 });
